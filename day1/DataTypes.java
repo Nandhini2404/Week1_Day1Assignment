2b32cc84865afff947f6a04a8ad34d112422ed95
Nandhini2404 <nandhinifame@gmail.com>
2026-01-03 21:33:30 +0530

    Add files via upload
    Assignment1

---
+++ day1/DataTypes.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName ="firefox";
+		Boolean isVisible =true;
+		short releaseYear = 1998;
+		char browserLogo= 'f';
+		System.out.println(browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo);
+
+	}
+
+}
